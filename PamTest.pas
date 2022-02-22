854163b73e444f81f72df7def5de0b754341a6f7
RedCatN1 <56513307+RedCatN1@users.noreply.github.com>
2022-02-22 17:22:56 +0500

    Update PamTest.pas

--- PamTest.pas
+++ PamTest.pas
@@ -1,24 +1,27 @@
 program PamTest;
-uses
-  Classes, SysUtils, pam_header;
 
+uses
+  Classes, SysUtils, pam_header, unit1;
 var
 res:integer;
 pam_hannn:Ppam_handle_t;
 TestConv:pam_conv_t;
-username:string;
-
+UserAccountData:PAMUserData;
 begin
-TestConv.conv:=@misc_conv;
-TestConv.appdata_ptr:=nil;
-writeln('PamTest, enter user name : ');
-readln(username);
-res:=pam_start(Pbyte(PChar('check')),Pbyte(Pchar(username)),@TestConv,@pam_hannn);
+UserAccountData.PamUName:='user';
+UserAccountData.PamUPass:='pass';
+TestConv.conv:=@CatConversation;
+TestConv.appdata_ptr:=@UserAccountData;
+res:=pam_start(PChar('check'),nil,@TestConv,@pam_hannn);
 writeln('pam_start result is : '+IntToStr(res));
 if res=PAM_SUCCESS then
-res:=pam_authenticate(pam_hannn,PAM_SILENT);
+res:=pam_authenticate(pam_hannn,0);
 writeln('pam_authenticate result is : '+IntToStr(res));
+if res=PAM_SUCCESS then
+res:=pam_acct_mgmt(pam_hannn, 0);
+writeln('pam_acct_mgmt result is : '+IntToStr(res));
 res:=pam_end(pam_hannn,res);
 writeln('pam_end result is : '+IntToStr(res));
 end.
+
 
